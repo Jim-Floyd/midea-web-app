f18e34b0c8e03f4168d4330c1d5b0c67ddb384bb
Jim-Floyd <jamshid.elboev5111995@gmail.com>
2023-12-07 14:58:41 +0500

    accepting data from google sheet

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import FetchCSVData from './g-sheet-db/g-sheet-models'
+import FetchCSVDataMan from './g-sheet-db/g-sheet-managers'
+
+
+
+const App = () => {
+  return (
+    <>
+      <h1>App</h1>
+     {/* <FetchCSVData/> */}
+     <FetchCSVDataMan/>
+     
+    
+     
+      
+    </>
+  )
+}
+
+export default App
